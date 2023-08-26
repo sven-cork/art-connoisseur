32922762bcf87aa08d15b6e8eba2394e4c54f53a
sven-cork <linslus81@gmail.com>
2023-08-26 16:42:35 +0000

    Adjust admin

--- materials/admin.py
+++ materials/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Material, Category
 
 # Register your models here.
-admin.site.register(Material)
-admin.site.register(Category)
+
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Material, MaterialAdmin)
+admin.site.register(Category, CategoryAdmin)
