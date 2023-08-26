0932ffb01c7c0d48ff427289cf638c570a4ec18f
sven-cork <linslus81@gmail.com>
2023-08-26 17:07:53 +0000

    Add materials view and templates

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Material
 
-# Create your views here.
+
+def all_materials(request):
+    """ View for all art materials with sort and search """
+
+    materials = Material.objects.all()
+
+    context = {
+        'materials': materials,
+    }
+
+    return render(request, 'materials/materials.html', context)
