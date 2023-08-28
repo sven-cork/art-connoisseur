5b3b247f89dc53ca85e55f2860649e1543ba4a9b
sven-cork <linslus81@gmail.com>
2023-08-28 12:34:32 +0000

    Implemented search functionality

--- materials/views.py
+++ materials/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Material
-
 
 def all_materials(request):
     """ View for all art materials with sort and search """
 
     materials = Material.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search criteria provided")
+                return redirect(reverse('materials'))
+            
+            queries = Q(name__icontains=query) | Q(type__icontains=query)
+            materials = materials.filter(queries)
 
     context = {
         'materials': materials,
+        'search_term': query,
     }
 
     return render(request, 'materials/materials.html', context)
