d99ab086dde7dfbc37612e1115680e26eee8c870
sven-cork <linslus81@gmail.com>
2023-09-01 13:13:04 +0000

    Add functionality for adding or removing product quantity

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,24 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from materials.models import Material
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        material = get_object_or_404(Material, pk=item_id)
+        total += quantity * material.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'material': material,
+        })
 
     if total < settings.FREE_DELIVERY_LIMIT:
         delivery = total * Decimal(settings.NORMAL_DELIVERY_PERCENTAGE / 100)
@@ -14,7 +27,7 @@ def bag_contents(request):
         delivery = 0
         free_delivery_delta = 0
 
-    total_charge = total + delivery
+    grand_total = total + delivery
     
     context = {
         'bag_items': bag_items,
@@ -23,7 +36,7 @@ def bag_contents(request):
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_limit': settings.FREE_DELIVERY_LIMIT,
-        'total_charge': total_charge,
+        'grand_total': grand_total,
     }
 
     return context
