adfd5322338639d641d25ad20f4d1afc5c2781e5
sven-cork <linslus81@gmail.com>
2023-09-08 10:26:03 +0000

    Add profiles app with functionality

--- art_connoisseur/urls.py
+++ art_connoisseur/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('materials/', include('materials.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
