2e214156460aa357668b3cfdd3f9574f6ba5b010
sven-cork <linslus81@gmail.com>
2023-09-22 13:05:12 +0000

    Adjust events.html for modal window comment submission

--- events/urls.py
+++ events/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('events/', views.events, name='events'),
     path('submit_comment/', views.submit_comment, name='submit_comment'),
+    path('load_comments/', views.load_comments, name='load_comments'),
+    path('event_detail/<int:event_id>/', views.event_detail, name='event_detail'),  # Add this line
 ]
-
--- events/views.py
+++ events/views.py
@@ -1,23 +1,47 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Event, Comment
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from django.views.decorators.http import require_POST
+
+def event_detail(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    comments = event.comments.all()
+    return render(request, 'events/event_detail.html', {'event': event, 'comments': comments})
+
+
+def load_comments(request):
+    event_id = request.GET.get('event_id')
+    event = Event.objects.get(id=event_id)
+    comments = event.comments.all()
+    comments_html = render_to_string('events/comments.html', {'comments': comments})
+
+    return JsonResponse({'comments_html': comments_html})
+
 
 def events(request):
-    # Fetch events and their related comments
     events = Event.objects.all()
-    print(events)  # Add this line for debugging
     return render(request, 'events/events.html', {'events': events})
 
-@login_required
+
 def submit_comment(request):
     if request.method == 'POST':
         event_id = request.POST.get('event_id')
-        event = Event.objects.get(id=event_id)
+        event = get_object_or_404(Event, id=event_id)
         comment_content = request.POST.get('comment')
 
         comment = Comment(content=comment_content, user=request.user, event=event)
         comment.save()
 
-        return redirect('events')
-    else:
-        return render(request, 'comment_form.html')  
+        comment_data = {
+            'content': comment.content,
+            'user': comment.user.username,
+            'timestamp': comment.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+        }
+
+        return JsonResponse({'comment': comment_data})
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
+
