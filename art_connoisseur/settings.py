57ce5153d6f2bfaadb2786977f77d35f0717dcd8
sven-cork <linslus81@gmail.com>
2023-09-29 14:15:57 +0000

    Add AWS cache control

--- art_connoisseur/settings.py
+++ art_connoisseur/settings.py
@@ -155,6 +155,12 @@ FREE_DELIVERY_LIMIT = 50
 NORMAL_DELIVERY_PERCENTAGE = 10
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'art-connoisseur'
     AWS_S3_REGION_NAME = 'eu-west-1'
