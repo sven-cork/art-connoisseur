41f0a91bf62adda445521afb71a698cf90ae8b10
sven-cork <linslus81@gmail.com>
2023-10-01 15:35:42 +0000

    Final commit

--- art_connoisseur/urls.py
+++ art_connoisseur/urls.py
@@ -18,4 +18,4 @@ urlpatterns = [
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'boutique_ado.views.handler404'
+handler404 = 'art_connoisseur.views.handler404'
